36941619e4813f909d779bfd061569c0f66f4b4b
vinod kunche <vinod.kunche@thetinydata.com>
2024-04-30 13:46:38 +0530

    scheduler-pagination-changes

--- src/app/features/schedulers/components/scheduler.service.ts
+++ src/app/features/schedulers/components/scheduler.service.ts
@@ -1,41 +1,37 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+
 @Injectable({
   providedIn: 'root'
 })
 export class SchedulerService {
-  createUser(value: any) {
-    throw new Error('Method not implemented.');
-  }
-
   private apiUrl = 'http://ec2-13-211-129-217.ap-southeast-2.compute.amazonaws.com:3001/scheduler';
   private localApiUrl = 'http://localhost:3001/scheduler';
   private media = 'http://localhost:3001/media';
-  screenId = '123';
+  private screens = 'http://localhost:3001/screen';
   private tenantId = '123456';
-  private screens ='http://localhost:3001/screen'
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   createScheduler(schedulerData: any): Observable<any> {
-    console.log('Creating user:', schedulerData);
     return this.http.post<any>(this.localApiUrl, schedulerData);
   }
-  
 
-  getScheduleByScreenId(screenIdName:string): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}?screenId=${screenIdName}`, );
+  getScheduleByScreenId(screenIdName: string): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}?screenId=${screenIdName}`);
   }
-  getSchedulers(): Observable<any[]> {
-    return this.http.get<any[]>(`${this.apiUrl}?tenantId=${this.tenantId}`, );
-}
 
-getVideos(): Observable<any[]> {
-  return this.http.get<any[]>(`${this.media}?tenantId=${this.tenantId}`, );
-}
+  getSchedulers(pageIndex: number, pageSize: number): Observable<any[]> {
+    const url = `${this.localApiUrl}?pageIndex=${pageIndex}&pageSize=${pageSize}&tenantId=${this.tenantId}`;
+    return this.http.get<any[]>(url);
+  }
 
-getScreensForTenant(): Observable<any> {
-  return this.http.get<any>(`${this.screens}/api/screens`);
-}
+  getVideos(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.media}?tenantId=${this.tenantId}`);
+  }
+
+  getScreensForTenant(): Observable<any> {
+    return this.http.get<any>(`${this.screens}/api/screens`);
+  }
 }
