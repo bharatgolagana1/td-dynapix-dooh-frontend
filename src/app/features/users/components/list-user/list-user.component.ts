9d2b893fed9aa4ba3f8615c2c1ee6082f5b8706d
Vinodkunchee <141106833+Vinodkunchee@users.noreply.github.com>
2024-05-01 17:02:14 +0530

    pagination-pageindex (#23)

--- src/app/features/users/components/list-user/list-user.component.ts
+++ src/app/features/users/components/list-user/list-user.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, ViewChild} from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { UserService } from '../../user.service';
 import { MatTableDataSource } from '@angular/material/table';
-import { MatPaginator } from '@angular/material/paginator';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+
 
 export interface User {
   userName: string;
@@ -17,23 +18,38 @@ export interface User {
   styleUrls: ['./list-user.component.scss']
 })
 export class ListUserComponent implements OnInit {
-  users:any = [];
   displayedColumns: string[] = ['userName', 'firstName', 'lastName', 'email', 'role', 'createdAt', 'updatedAt'];
   dataSource!: MatTableDataSource<User>;
+  totalUsers: number = 0;
+  pageIndex: number = 0;
+  pageSize: number = 0;
+  searchValue: string = '';
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   constructor(private userService: UserService) { }
 
   ngOnInit(): void {
-    this.userService.getUsers().subscribe((usersList: any) => {
-      if (usersList.users?.length > 0) {
-        this.users = usersList.users;
-        this.dataSource = new MatTableDataSource<User>(this.users);
-        this.dataSource.paginator = this.paginator;
-      }
+    this.loadUsers();
+  }
+
+  loadUsers(pageIndex: number = 0): void {
+    this.userService.getUsers(pageIndex + 1, this.pageSize, this.searchValue).subscribe((usersList: any) => {
+      this.dataSource = new MatTableDataSource<User>(usersList.users);
+      this.totalUsers = usersList.totalUsers;
+      this.pageIndex = usersList.pageIndex - 1;
+      this.dataSource.paginator = this.paginator;
     });
   }
 
- 
+  onPageChange(event: PageEvent): void {
+    this.loadUsers(event.pageIndex);
+  }
+
+  onSearch(event: any): void {
+    this.searchValue = event.target.value.trim().toLowerCase();
+    this.loadUsers();
+  }
 }
+
+
--- src/app/features/users/user.service.ts
+++ src/app/features/users/user.service.ts
@@ -1,24 +1,28 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
   private apiUrl = 'http://ec2-13-211-129-217.ap-southeast-2.compute.amazonaws.com:3001/users';
+  private localAPIUrl =  'http://localhost:3001/users';
   private tenantId = '123456';
 
   constructor(private http: HttpClient) { }
 
   createUser(userData: any): Observable<any> {
     console.log('Creating user:', userData);
-  return this.http.post<any>(`${this.apiUrl}/create`, { ...userData, tenantId: this.tenantId });
+    return this.http.post<any>(`${this.apiUrl}/create`, { ...userData, tenantId: this.tenantId });
   }
-  
-  getUsers(): Observable<any[]> {
-    return this.http.get<any[]>(`${this.apiUrl}?tenantId=${this.tenantId}`, );
+
+  getUsers(pageIndex: number, pageSize: number, search: string): Observable<any> {
+    const params = new HttpParams()
+      .set('tenantId', this.tenantId)
+      .set('pageIndex', pageIndex.toString())
+      .set('pageSize', pageSize.toString())
+      .set('search', search);
+    return this.http.get<any>(this.localAPIUrl, { params });
   }
- 
 }
