b4dacd32e75e95cfecf96a8f6a55bc07fe17f607
hemanth <hemanth.kommoji@thetinydata.com>
2024-05-02 14:12:09 +0530

    image-size-changes

--- src/app/features/schedulers/components/image-cards-list/image-cards-list.component.ts
+++ src/app/features/schedulers/components/image-cards-list/image-cards-list.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-image-cards-list',
   templateUrl: './image-cards-list.component.html',
-  styleUrls: ['./image-cards-list.component.scss']
+  styleUrls: ['./image-cards-list.component.scss'],
+  
 })
 export class ImageCardsListComponent {
+  slideConfig = {
+    "dots" : true, 
+    "autoplay" : true,
+    "autoplaySpeed" : 5000,
+    "pauseOnHover" : true,
+    "responsive"  :  [
+      {
+        "breakpoint"  : 992,
+        "settings"  : {
+          "arrows" : true,
+          "infinite" : true,
+          "slidesToShow" : 2,
+          "slidesToScroll" : 2,
+        }
+      },
+      {
+        "breakpoint"  : 768,
+        "settings"  : {
+          "arrows" : true,
+          "infinite" : true,
+          "slidesToShow" : 1,
+          "slidesToScroll" : 1,
+        }
+      }
+    ],
+    prevArrow: '<button type="button" class="slick-prev">Previous</button>',
+    nextArrow: '<button type="button" class="slick-next">Next</button>',
+  };
 
+  constructor(@Inject(MAT_DIALOG_DATA) public data: { images: string[] }) {}
 }
