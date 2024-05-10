90216838dc2d177362be89b32a8ed51635a4d33d
sarmak <sarmak@prospectatech.com>
2024-05-10 13:06:21 +0530

    upload-media-update

--- src/app/features/media/media.service.ts
+++ src/app/features/media/media.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpEventType } from '@angular/common/http';
+import { HttpClient, HttpEvent, HttpEventType, HttpResponse } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
@@ -9,39 +9,47 @@ export class MediaService {
 
   async uploadMedia(file: File): Promise<number> {
     const formData = new FormData();
-    formData.append('file', file);
+    formData.append('videoFiles', file);
 
     try {
-      const response = await this.http
-        .post<any>(
-          'http://ec2-13-211-129-217.ap-southeast-2.compute.amazonaws.com:3001/media',
-          formData,
-          {
-            reportProgress: true,
-            observe: 'events',
-          }
-        )
-        .toPromise();
+      const response = await this.http.post('http://localhost:3001/media', formData, {
+        reportProgress: true,
+        observe: 'events',
+        responseType: 'text',
+      }).toPromise();
 
       if (response && response.type === HttpEventType.UploadProgress) {
-        // Check if response.total is defined
-        if (response.total !== undefined && response.total > 0) {
-          // Upload progress event
-          return Math.round((100 * response.loaded) / response.total);
+        const progressEvent = response as HttpEvent<ProgressEvent>;
+        const progress = this.calculateUploadProgress(progressEvent);
+
+        return progress;
+      } else if (response instanceof HttpResponse) {
+        console.log('Upload response:', response);
+        const responseBody = response.body;
+        if (responseBody && responseBody.includes('Media uploaded successfully')) {
+          return 100;
         } else {
-          // Unable to calculate progress percentage
-          return -1;
+          throw new Error('Unexpected response from server');
         }
-      } else if (response && response.type === HttpEventType.Response) {
-        // Upload completed successfully
-        return 100;
       } else {
-        // Error occurred
-        throw new Error('Upload failed');
+        throw new Error('Unexpected event during upload');
       }
     } catch (error) {
       console.error('Error uploading file:', error);
       throw error;
     }
+  }
+
+  private calculateUploadProgress(event: HttpEvent<ProgressEvent>): number {
+    if (!event || event.type !== HttpEventType.UploadProgress) {
+      return 0;
+    }
+    const progressEvent = event as HttpEvent<ProgressEvent>;
+    const loaded = event?.loaded || 0;
+    const total = event?.total || 0;
+
+    const progress = Math.round((100 * loaded) / (total || 1));
+
+    return progress;
   }
 }
