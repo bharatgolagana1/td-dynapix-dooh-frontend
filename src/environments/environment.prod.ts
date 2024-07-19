3ccce4034993853edb4b0ed5fa2d171e1e425b2e
Bharat Yadav Golagana <bharat.golagana@gmail.com>
2024-07-19 14:56:44 +0530

    prod keycloak url updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
 export const environment = {
-    production: true,
-    baseApiUrl: 'http://localhost:3001',
-    keycloakUrl:'http://localhost:8080',
-    tenantId :  '123456'
-  };
-  
+  production: true,
+  baseApiUrl: 'http://localhost:3001',
+  keycloakUrl: 'https://keycloak.dynapix.in',
+  tenantId: '123456',
+};
